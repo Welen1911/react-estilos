7ec4077ec2e7e539186000c993ac2adf162643eb
Welen1911 <welenalmeida2210@gmail.com>
2023-07-14 15:27:07 -0300

    Testando o bootstrap no react e primeira aula

---
+++ src/Botao.tsx
@@ -0,0 +1,13 @@
+import { isAbsolute } from "path"
+
+export const Botao = () => {
+    return (
+        <div style={{
+            display: "grid",
+            justifyContent: "center",
+            alignContent: "center"
+        }}>
+            <button className={"btn btn-dark mt-5"}>Testando Bootstrap</button>
+            </div>
+    )
+}
---
+++ src/Componente.tsx
@@ -0,0 +1,7 @@
+export const Componente = () => {
+    return (
+       <nav className="nav navbar bg-dark"><div className="header text-white"><h3 className="h3" style={{
+        marginLeft: "20px"
+       }}>Título</h3></div></nav>
+    )
+}
